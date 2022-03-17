87b325ab896504e387d884608d953bc605bac3cb
Benjamin van de Vorstenbosch <bencvdv@gmail.com>
2022-03-17 12:34:47 +1100

    Client connection and authentication with ds-sim

--- src/Client.java
+++ src/Client.java
@@ -1,16 +1,22 @@
 import java.net.*;
+import java.rmi.UnexpectedException;
 import java.io.*;
 
 public class Client {
-    static Socket s;
-    static DataInputStream din;
-    static DataOutputStream dout;
-    static BufferedReader br;
+    static final int NUM_ARGS = 2;
+    static Client client;
+
+    Socket s;
+    BufferedReader din;
+    DataOutputStream dout;
+    BufferedReader br;
 
     public static void main(String[] args) {
-        if(args.length != 2) {
+        if(args.length < NUM_ARGS) {
             System.out.println("Please provide a host name (localhost) and port number (6666)");
             System.exit(0);
+        } else if(args.length > NUM_ARGS) {
+            System.out.println("Too many arguments supplied! Only the first " + NUM_ARGS + " will be used.");
         }
 
         String hostname = args[0];
@@ -24,32 +30,75 @@ public class Client {
             System.out.println("Using system ports. Make sure this is what you want!");
         }
 
-        try{      
-            Socket s = new Socket(hostname, port);
+        Client client = new Client();
+        
+        try{
+            client.connect(hostname, port);
 
-            din = new DataInputStream(s.getInputStream());  
-            dout = new DataOutputStream(s.getOutputStream());  
-            br = new BufferedReader(new InputStreamReader(System.in));  
-
-            System.out.println("Connection with server established.");
-
-            String message = "";
-            String receivedMessage = "";
-
-            while(!message.equals("BYE")) {
-                System.out.println("What would you like to say? (write HELO first and then BYE for this task)\n");
-                message = br.readLine();
-                dout.writeUTF(message);
-                dout.flush();
-
-                receivedMessage = din.readUTF();
-                System.out.println("Server responded: " + receivedMessage);
+            if(!client.confirmConnection()) {
+                throw new UnexpectedException("Server was unable to handle authentication!");
             }
-            
-            din.close();
-            s.close();
+
+            client.runJobs();
+            client.disconnect();
+
         } catch(Exception e) {
             System.out.println(e);
-        }  
+        }       
+    }
+
+    public void connect(String hostname, int port) throws IOException {
+        s = new Socket(hostname, port);
+        din = new BufferedReader(new InputStreamReader(s.getInputStream()));  
+        dout = new DataOutputStream(s.getOutputStream());
+
+        System.out.println("Connection with server established.");
+    }
+
+    public void disconnect() throws IOException {
+        din.close();
+        s.close();
+    }
+
+    public boolean confirmConnection() throws IOException {
+
+        String messages[] = {
+            "HELO", 
+            "AUTH " + System.getProperty("user.name")
+        };
+
+        String reply = "";
+
+        for(String m : messages) {
+            reply = handleMessage(m);
+
+            if(!reply.equals("OK")) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void runJobs() throws IOException {
+        // This is where the client will be sent jobs to distribute
+        handleMessage("REDY");
+        handleMessage("QUIT");
+    }
+
+    public String handleMessage(String message) throws IOException {
+        dout.write(formatOutput(message));
+        dout.flush();
+
+        System.out.println("SENT " + message);
+
+        String receivedMessage = din.readLine();
+        System.out.println("RCVD " + receivedMessage);
+
+        return receivedMessage;
+    }
+
+    public static byte[] formatOutput(String s) {
+        return (s + "\n").getBytes();
     }
 }
