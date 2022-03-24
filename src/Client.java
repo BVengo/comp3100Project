dea8b451406f4edcdf52c90782305fc6ffc9bd7d
Benjamin van de Vorstenbosch <bencvdv@gmail.com>
2022-03-24 18:26:47 +1100

    Functional LRR client, with a few inconsistencies

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,9 @@
 import java.net.*;
 import java.rmi.UnexpectedException;
+import java.util.HashMap;
 import java.io.*;
+
+// TODO: Remove abundant exception usage, and have proper error handling.
 
 public class Client {
     static final int NUM_ARGS = 2;
@@ -9,6 +12,8 @@ public class Client {
     BufferedReader din;
     DataOutputStream dout;
     BufferedReader br;
+
+    HashMap<String, Server> servers = new HashMap<String, Server>();
 
     public static void main(String[] args) {
         if(args.length < NUM_ARGS) {
@@ -33,12 +38,9 @@ public class Client {
         
         try{
             client.connect(hostname, port);
-
-            if(!client.confirmConnection()) {
-                throw new UnexpectedException("Server was unable to handle authentication!");
-            }
-
-            client.runJobs();
+            client.authenticateConnection();
+            client.storeServerDetails();
+            client.sendReady();
             client.disconnect();
 
         } catch(Exception e) {
@@ -50,105 +52,140 @@ public class Client {
         s = new Socket(hostname, port);
         din = new BufferedReader(new InputStreamReader(s.getInputStream()));  
         dout = new DataOutputStream(s.getOutputStream());
-
-        System.out.println("Connection with server established.");
     }
 
-    public void disconnect() throws IOException {
+    public void disconnect() throws Exception {
+        handleMessage("QUIT", "QUIT");
         din.close();
         s.close();
     }
 
-    public boolean confirmConnection() throws IOException {
+    public void authenticateConnection() throws Exception {
 
         String messages[] = {
             "HELO", 
             "AUTH " + System.getProperty("user.name")
         };
 
-        String reply = "";
-
         for(String m : messages) {
-            reply = handleMessage(m);
-
-            if(!reply.equals("OK")) {
-                return false;
-            }
+            handleMessage(m, "OK");
         }
-
-        return true;
     }
 
-    public void runJobs() throws IOException {
-        // This is where the client will be sent jobs to distribute
+    public void storeServerDetails() throws Exception {
+        handleMessage("REDY");
+
+        String reply = handleMessage("GETS All");
+        String[] dataDetails = reply.split(" ");
+
+        int numServers = Integer.parseInt(dataDetails[1]);
+        
+        for(int i = 0; i < numServers; i++) {
+            reply = handleMessage(i == 0 ? "OK" : "");
+
+            Server s = getServerFromString(reply); 
+            servers.put(s.getKey(), s);
+        }
+
+        handleMessage("OK", ".");
+    }
+
+    public void sendReady() throws Exception {
         String reply = handleMessage("REDY");
         String[] splitReply = reply.split(" ");
 
-        if(!splitReply[0].equals("JOBN")) {
-            // TODO: Handle other commands at this point, or branch for different commands
-            return;
+        switch(splitReply[0]) {
+            case "JOBN":
+                Job job = getJobFromArray(splitReply);
+                distributeJob(job);
+                return;
+            case "JCPL":
+                sendReady();
+                return;
+            case "NONE":
+                return;
+            default:
+                return;
         }
-        // See Job.java for descriptions
-        int submitTime  = Integer.parseInt(splitReply[1]);
-        int jobId       = Integer.parseInt(splitReply[2]);
-        int estRuntime  = Integer.parseInt(splitReply[3]);
-        int core        = Integer.parseInt(splitReply[4]);
-        int memory      = Integer.parseInt(splitReply[5]);
-        int disk        = Integer.parseInt(splitReply[6]);
+    }
 
-        Job job = new Job(submitTime, jobId, estRuntime, core, memory, disk);
+    public void distributeJob(Job job) throws Exception {
+        String reply = handleMessage("GETS Capable " + job.core + " " + job.memory + " " + job.disk);
+        String[] dataDetails = reply.split(" ");
 
-        System.out.println(job);
-
-        // TODO: Example log only has capable, but I need to explore further
-        reply = handleMessage("GETS Capable " + core + " " + memory + " " + disk);
-        splitReply = reply.split(" ");
-
-        if(!splitReply[0].equals("DATA")) {
-            // TODO: Handle other commands at this point, or branch for different commands
-            return;
-        }
-
-        int numJobs         = Integer.parseInt(splitReply[1]);
-        int messageLength   = Integer.parseInt(splitReply[2]);
-
-        Server[] servers = new Server[numJobs];
+        int numServers = Integer.parseInt(dataDetails[1]);
 
         reply = handleMessage("OK");
-        splitReply = reply.split(" ");
-        
-        for(int i = 0; i < numJobs; i++) {
-            String serverName   = splitReply[0];
-            int serverId        = Integer.parseInt(splitReply[1]);
-            String serverState  = splitReply[2];
-            int curStartTime    = Integer.parseInt(splitReply[3]);
-            core                = Integer.parseInt(splitReply[4]);
-            memory              = Integer.parseInt(splitReply[5]);
-            disk                = Integer.parseInt(splitReply[6]);
-            int wJobs           = Integer.parseInt(splitReply[7]);
-            int rJobs           = Integer.parseInt(splitReply[8]);
+        Server largestServer = new Server();
+        int maxTotalCores = -1;
 
-            servers[i] = new Server(serverName, serverId, serverState, curStartTime, core, memory, disk, wJobs, rJobs);
+        for(int i = 0; i < numServers; i++) {
+            Server s = getServerFromString(reply);
+            String key = s.getKey();
+
+            int totalCores = servers.get(key).core;
             
-            if(i != numJobs - 1) {
-                reply = din.readLine();
+            if(i == 0 || totalCores > maxTotalCores || (totalCores == maxTotalCores && s.compareTo(largestServer) > 0)) {
+                largestServer = s;
+                maxTotalCores = totalCores;
+            }
+            
+            if(i != numServers - 1) {
+                reply = handleMessage("");
             }
         }
 
-        handleMessage("OK");
-        handleMessage("QUIT");
+        handleMessage("OK", ".");
+        handleMessage("SCHD " + job.jobId + " " + largestServer.serverName + " " + largestServer.serverId, "OK");
+
+        sendReady();
     }
 
-    public String handleMessage(String message) throws IOException {
-        dout.write(formatOutput(message));
-        dout.flush();
+    public Job getJobFromArray(String[] jobDetails) {
+        int submitTime  = Integer.parseInt(jobDetails[1]);
+        int jobId       = Integer.parseInt(jobDetails[2]);
+        int estRuntime  = Integer.parseInt(jobDetails[3]);
+        int core        = Integer.parseInt(jobDetails[4]);
+        int memory      = Integer.parseInt(jobDetails[5]);
+        int disk        = Integer.parseInt(jobDetails[6]);
 
-        System.out.println("SENT " + message);
+        return(new Job(submitTime, jobId, estRuntime, core, memory, disk));
+    }
 
+    public Server getServerFromString(String s) {
+        String[] serverDetails = s.split(" ");
+        
+        String serverName   = serverDetails[0];
+        int serverId        = Integer.parseInt(serverDetails[1]);
+        String serverState  = serverDetails[2];
+        int curStartTime    = Integer.parseInt(serverDetails[3]);
+        int core                = Integer.parseInt(serverDetails[4]);
+        int memory              = Integer.parseInt(serverDetails[5]);
+        int disk                = Integer.parseInt(serverDetails[6]);
+        int wJobs           = Integer.parseInt(serverDetails[7]);
+        int rJobs           = Integer.parseInt(serverDetails[8]);
+
+        return(new Server(serverName, serverId, serverState, curStartTime, core, memory, disk, wJobs, rJobs));
+    }
+
+    public String handleMessage(String message) throws Exception {
+        if(!message.equals("")) {
+            dout.write(formatOutput(message));
+            dout.flush();
+        }
+        
         String receivedMessage = din.readLine();
-        System.out.println("RCVD " + receivedMessage);
-
         return receivedMessage;
+    }
+
+    public String handleMessage(String message, String expectedResponse) throws Exception {
+        String reply = handleMessage(message);
+
+        if(!reply.equals(expectedResponse)) {
+            throw new UnexpectedException("Didn't received an expected reply!");
+        }
+
+        return reply;
     }
 
     public static byte[] formatOutput(String s) {
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,4 @@
-public class Server {
+public class Server implements Comparable<Server> {
     String serverName;
     int serverId;
     String serverState;
@@ -8,6 +8,10 @@ public class Server {
     int disk;
     int wJobs;
     int rJobs;
+
+    String[] statePriority = {"idle", "inactive", "booting", "active"};
+
+    public Server() {};
 
     public Server(String serverName, int serverId, String serverState, int curStartTime, int core, int memory, int disk, int wJobs, int rJobs) {
         this.serverName = serverName;
@@ -21,7 +25,55 @@ public class Server {
         this.rJobs = rJobs;
     }
 
+    public String getKey() {
+        return serverName + "-" + serverId;
+    }
+
     public String toString() {
-        return serverName + ", " + serverId + ", " + serverState + ", " + curStartTime + ", " + core + ", " + memory + ", " + disk + ", " + wJobs + ", " + rJobs;
-    }    
+        return getKey() + ", " + serverName + ", " + serverId + ", " + serverState + ", " + curStartTime + 
+                          ", " + core + ", " + memory + ", " + disk + ", " + wJobs + ", " + rJobs;
+    }
+
+    @Override
+    public int compareTo(Server o) {
+        int c;
+        
+        // Prioritise less running jobs
+        c = Integer.compare(o.rJobs, rJobs);
+        if(c != 0) {
+            return c;
+        }
+        
+        // Prioritise less working jobs
+        c = Integer.compare(o.wJobs, wJobs);
+        if(c != 0) {
+            return c;
+        }
+
+        int idx1 = -1;
+        int idx2 = -1;
+
+        for(int i = 0; i < statePriority.length; i++) {
+            if(statePriority[i].equals(this.serverState)) {
+                idx1 = i;
+            }
+
+            if(statePriority[i].equals(o.serverState)) {
+                idx2 = i;
+            }
+        }
+
+        // Prioritise in array from left to right
+        c = Integer.compare(idx2, idx1);
+        if(c != 0) {
+            return c;
+        }
+        
+        // Prioritise smaller startTime
+        c = Integer.compare(o.curStartTime, curStartTime);
+        if(c != 0) {
+            return c;
+        }
+        return 0;
+    }
 }
